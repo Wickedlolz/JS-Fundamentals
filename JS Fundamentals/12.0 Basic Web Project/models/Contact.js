c5d526f29b3f3e7259debee38d55620d06939c5c
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2021-08-11 19:57:53 +0300

    adding Contact model

--- JS Fundamentals/12.0 Basic Web Project/models/Contact.js
+++ JS Fundamentals/12.0 Basic Web Project/models/Contact.js
@@ -1,1 +1,10 @@
 // TODO: Create Contact model and export it
+
+class Contact {
+    constructor(name, phone) {
+        this.name = name;
+        this.phone = phone;
+    }
+}
+
+module.exports = Contact;
