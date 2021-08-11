f1c5250453f4aac87b8469edbc455a46b5f2dee1
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2021-08-11 19:53:09 +0300

    removing body-parser package

--- JS Fundamentals/12.0 Basic Web Project/index.js
+++ JS Fundamentals/12.0 Basic Web Project/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const path = require('path');
 const handlebars = require('express-handlebars');
-const bodyParser = require('body-parser');
 const routing = require('./routing');
 const port = 3000;
+
 const app = express();
 
 app.engine(".hbs", handlebars({
@@ -12,9 +12,10 @@ app.engine(".hbs", handlebars({
 app.set('view engine', '.hbs');
 app.set('views', path.join(__dirname, '/views'));
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({
+app.use(express.urlencoded({
     extended: true
 }));
+
 routing(app);
 
 app.listen(port, console.log("Listening on port " + port + "..."));
