386d600c647ddd93e9ab179ac063d474ef7fa093
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2021-08-11 19:56:06 +0300

    create phonebook array and methods to add contact in array and get it

--- JS Fundamentals/12.0 Basic Web Project/phonebook.js
+++ JS Fundamentals/12.0 Basic Web Project/phonebook.js
@@ -1,5 +1,20 @@
 /* TODO: 
-	create phonebook array
-	add methods for adding in the phonebook and getting it
-	export the methods
+    create phonebook array
+    add methods for adding in the phonebook and getting it
+    export the methods
 */
+
+let phonebook = [];
+
+function addContact(contact) {
+    phonebook.push(contact);
+}
+
+function getContacts() {
+    return phonebook;
+}
+
+module.exports = {
+    addContact,
+    getContacts
+}
